656710ea843ba86708bfc8c36e5795af101fbff8
jesus <jesuspec9@icloud.com>
2022-11-05 13:03:32 -0500

    .

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+   let wordToArray= word.split("");
+   return wordToArray.reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+ 
