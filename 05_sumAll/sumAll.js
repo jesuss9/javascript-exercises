8c615876af2f4f0a350a28df4761a308a5491e3b
jesus <jesuspec9@icloud.com>
2022-12-06 18:02:21 -0600

    .

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, z) {
+ let biggerVal = Math.max (x, z);
+ let smallerVal = Math.min (x, z);
+ let finalSum = 0;
+ if ((typeof x ==="string") || (typeof z === "string")) {
+    return "ERROR";
+ } else if ((typeof x === "object") || (typeof z === "object")) {
+    return "ERROR";
+ } else if ((biggerVal >= 0) && (smallerVal >= 0)) {
+    for (let i = smallerVal; i <= biggerVal; i++) {
+    finalSum = finalSum + i;
+    }     
+ } else {
+    return "ERROR";
+ };
+ return finalSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+ 
